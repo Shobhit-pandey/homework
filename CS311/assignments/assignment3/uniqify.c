5a2abae51b2cfd07755070d5f907055fc017e2a9
Trevor Bramwell <trevor@bramwell.net>
2013-11-04 22:28:42 -0800

    CS311: Add initial source and header for uniqify.

---
+++ CS311/assignments/assignment3/uniqify.c
@@ -0,0 +1,10 @@
+/*
+ * CS311: Project 3 - Uniqify
+ *
+ * Author: Trevor Bramwell
+ */
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
---
+++ CS311/assignments/assignment3/uniqify.h
@@ -0,0 +1,8 @@
+/*
+ * CS311: Project 3 - Uniqify
+ *
+ * Author: Trevor Bramwell
+ */
+
+#define TRUE 0
+#define FALSE 1
