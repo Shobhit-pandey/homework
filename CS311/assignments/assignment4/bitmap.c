7e9a44e96f8e6bec5e3fe0b8af90bb663eacb2dd
Trevor Bramwell <trevor@bramwell.net>
2013-11-26 01:15:32 -0800

    CS311: Fix issue with integer overflow.
    Add mutex locking for whole unsigned int.

--- CS311/assignments/assignment4/bitmap.c
+++ CS311/assignments/assignment4/bitmap.c
@@ -1,61 +1,81 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 #include <stdint.h>
 #include <pthread.h>
 
 #include "bitmap.h"
 
 //#define MAX (UINT_MAX)
-#define MAX 100
-#define NUM_THREADS 3
+//#define MAX 100
 
+static long MAX;
 static uint8_t *bitarray;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void *sieve(void *k)
 {
-    uint32_t i = (uint32_t) k;
-    uint32_t j;
+    int ret;
 
-    for(j = i + i; j < MAX; j += i) {
-        BITSET(bitarray, j);
+    for (long i = 2; i < ceil(sqrt(MAX)); ++i) {
+        // Mark all multiples of the number
+        if (!BITTEST(bitarray, i)) {
+            ret = pthread_mutex_lock(&mutex);
+            if (ret != 0) errExit("pthread_mutex_lock"); 
+
+            for(long j = i + i; 0 < j && j < MAX; j += i) {
+                //bitarray[BITSLOT(j)]
+                BITSET(bitarray, j);
+            }
+
+            ret = pthread_mutex_unlock(&mutex);
+            if (ret != 0) errExit("pthread_mutex_lock"); 
+        }
     }
 
-    return 0;
+    return NULL;
 }
 
-int main()
+int main(int argc, char **argv)
 {
-    uint32_t i;
-    pthread_t threads[NUM_THREADS];
+    int ret;
+    long i = 0;
+
+    int NUM_THREADS = atoi(argv[1]);
+    MAX = atoi(argv[2]);
+
+    pthread_t *threads = (pthread_t *) malloc(NUM_THREADS * sizeof(pthread_t));
+    if (threads == 0) errExit("malloc threads");
+
+    fprintf(stderr, "BITNSLOTS = %ld\n", (long) BITNSLOTS(MAX));
 
     bitarray = (uint8_t *) malloc( BITNSLOTS(MAX) * sizeof(uint8_t));
-    if (bitarray == 0) {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
+    if (bitarray == 0) errExit("malloc bitarray");
 
     memset(bitarray, 0, BITNSLOTS(MAX));
 
-    for (int i = 0; i < NUM_THREADS; ++i) {
-        if (!BITTEST(bitarray, (i+2))) {
-            pthread_create(&threads[i],
-                           NULL,
-                           sieve,
-                           (void *) (i+2));
-        }
+    for (i = 0; i < NUM_THREADS; ++i) {
+        ret = pthread_create(&threads[i],
+                             NULL,
+                             sieve,
+                             NULL);
+        if (ret != 0) errExit("pthread_create");
     }
 
     for (int j = 0; j < NUM_THREADS; ++j) {
-        pthread_join(threads[j], NULL);
+        ret = pthread_join(threads[j], NULL);
+        if (ret != 0) errExit("pthread_join");
     }
 
+    FILE *file = fopen("output.txt", "w");
     for (i = 2; i < MAX; ++i) {
         if(!BITTEST(bitarray, i)) {
-            printf("%u\n", i);
+            fprintf(file, "%ld\n", i);
         }
     }
 
+    free(threads);
     free(bitarray);
 
     return 0;
--- CS311/assignments/assignment4/bitmap.h
+++ CS311/assignments/assignment4/bitmap.h
@@ -4,9 +4,14 @@
 
 #include <limits.h>     /* for CHAR_BIT */
 
-#define BITMASK(b) (1 << ((b) % CHAR_BIT))
-#define BITSLOT(b) ((b) / CHAR_BIT)
+#define BITMASK(b) (1 << ((b) % CHAR_BIT)) /* Index of b in byte */
+#define BITSLOT(b) ((b) / CHAR_BIT) /* Index of byte containing b in bitmap */
 #define BITSET(a, b) ((a)[BITSLOT(b)] |= BITMASK(b))
 #define BITCLEAR(a, b) ((a)[BITSLOT(b)] &= ~BITMASK(b))
 #define BITTEST(a, b) ((a)[BITSLOT(b)] & BITMASK(b))
-#define BITNSLOTS(nb) ((nb - CHAR_BIT) / CHAR_BIT)
+#define BITNSLOTS(nb) ((nb) / CHAR_BIT)
+
+void errExit(char *err) {
+    perror(err);
+    exit(EXIT_FAILURE);
+}
