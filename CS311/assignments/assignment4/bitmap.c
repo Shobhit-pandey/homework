b90c9f75ea1585723dacddd733f3f716f0c688f3
Trevor Bramwell <trevor@bramwell.net>
2013-11-26 22:34:14 -0800

    CS311: Don't do extra work.
    Keeps track of sieve elements, and checks to make sure there aren't any
    multiples already being checked.

--- CS311/assignments/assignment4/bitmap.c
+++ CS311/assignments/assignment4/bitmap.c
@@ -8,16 +8,27 @@
 #include "bitmap.h"
 
 static long MAX;
+static int NUM_THREADS;
 static uint8_t *bitarray;
+static long* working;
 pthread_mutex_t *mutexes;
 
 void *sieve(void *k)
 {
-    int ret;
+    int ret = 0;
+    int tid = (int)k;
 
     for (long i = 2; i < ceil(sqrt(MAX)); ++i) {
         // Mark all multiples of the number
         if (!BITTEST(bitarray, i)) {
+            // Make sure the number we are using is not divisible by
+            // any other number currently being processed.
+            for (int v = 0; v < NUM_THREADS; ++v) {
+                if((i % working[v]) == 0) continue;
+            }
+
+            working[tid] = i;
+
             for(long j = i + i; 0 < j && j < MAX; j += i) {
                 ret = pthread_mutex_lock(&mutexes[BITSLOT(j)]);
                 if (ret != 0) errExit("pthread_mutex_lock"); 
@@ -38,13 +49,20 @@ int main(int argc, char **argv)
     int ret;
     long i = 0;
 
-    int NUM_THREADS = atoi(argv[1]);
+    NUM_THREADS = atoi(argv[1]);
     MAX = atoi(argv[2]);
 
     pthread_t *threads = (pthread_t *) malloc(NUM_THREADS * sizeof(pthread_t));
     if (threads == 0) errExit("malloc threads");
 
-    mutexes = (pthread_mutex_t *) malloc( BITNSLOTS(MAX) * sizeof(pthread_mutex_t));
+    working = (long *) malloc(NUM_THREADS * sizeof(long));
+    if (working == 0) errExit("malloc working");
+
+    for (int i = 0; i < NUM_THREADS; ++i) {
+        working[i] = 1;
+    }
+    
+    mutexes = (pthread_mutex_t *) malloc((BITNSLOTS(MAX)+1) * sizeof(pthread_mutex_t));
     if (mutexes == 0) errExit("malloc mutexes");
 
     fprintf(stderr, "BITNSLOTS = %ld\n", (long) BITNSLOTS(MAX));
@@ -63,7 +81,7 @@ int main(int argc, char **argv)
         ret = pthread_create(&threads[i],
                              NULL,
                              sieve,
-                             NULL);
+                             (void *)i);
         if (ret != 0) errExit("pthread_create");
     }
 
@@ -84,6 +102,7 @@ int main(int argc, char **argv)
         }
     }
 
+    free(working);
     free(threads);
     free(mutexes);
     free(bitarray);
