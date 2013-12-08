f4bdd9449f08bee9b8b3b5c3756a7125ce860d18
Trevor Bramwell <trevor@bramwell.net>
2013-12-08 14:06:21 -0800

    CS311: Add initial XML expected response to report

--- CS311/assignments/assignment5/report.py
+++ CS311/assignments/assignment5/report.py
@@ -5,16 +5,30 @@ Basic client code used from: http://pymotw.com/2/select/
 import socket
 import sys
 
-messages = ['results',
-            'quit']
-server_address = ('localhost', 50000)
+messages = ['results', 'die']
+reponse = [
+    ('<?xml version="1.0" encoding="UTF-8"?>'
+     '<perfect><number>1</number><number>6</number></perfect>'
+     '<tested><min>1</min><max>35234234</max></tested>'
+     '<clients>'
+     '<client><ip>127.0.0.1</ip><port>53234</port><perf>53234</perf></client>'
+     '<client><ip>127.0.0.1</ip><port>53524</port><perf>23334</perf></client>'
+     '</clients>')
+]
+HOST = 'localhost'
+PORT = 50000
+server_address = (HOST, PORT)
 
 # Create a TCP/IP socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
 print >>sys.stderr, 'Connecting to %s port %s' % server_address
-s.connect(server_address)
+try:
+    s.connect(server_address)
+except socket.error:
+    print >>sys.stderr, 'Connection refused'
+    exit(1)
 
 for message in messages:
     # Send messages on both sockets
