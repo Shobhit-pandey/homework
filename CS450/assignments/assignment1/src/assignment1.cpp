a68089bce144020049c55490841011a8646feb26
Trevor Bramwell <trevor@bramwell.net>
2014-10-12 20:46:41 -0700

    Add Assignment1 Comments

--- CS450/assignments/assignment1/src/assignment1.cpp
+++ CS450/assignments/assignment1/src/assignment1.cpp
@@ -1,6 +1,12 @@
 // SimpleProgram.cpp
 //
 // A simple 2D OpenGL program
+//
+// Assignment: 1
+// Class: CS450 - Introduction to Computer Graphics
+// Author: Trevor Bramwell
+// Date: 10 October 2014
+//
 
 #define GL3_PROTOTYPES
 
@@ -8,13 +14,14 @@
 // macro definitions.
 #include "../include/Angel.h"
 
-// A global constant for the number of points that will be in our object.
+// A global constant for the number of points that will be in each of
+// the object.
 const int TriPoints = 3;
 const int RectPoints = 6;
 const int CoorPoints = 4;
 const int PointPoints = 5;
 
-// 2 Lines
+// Lines for the R2 Coordinate Plane
 vec2 coor_plane[] = {
     vec2(-1, 0),
     vec2(1, 0),
@@ -22,6 +29,7 @@ vec2 coor_plane[] = {
     vec2(0, 1)
 };
 
+// Colors for the R2 Coordinate Plane
 vec4 coor_colors[] = {
     vec4(1.0, 1.0, 1.0, 1.0),
     vec4(1.0, 1.0, 1.0, 1.0),
@@ -29,7 +37,7 @@ vec4 coor_colors[] = {
     vec4(1.0, 1.0, 1.0, 1.0)
 };
 
-// 3 Points
+// Some Random Points within R2
 vec2 points[] = {
     vec2(-0.25, 0.75),
     vec2(0.75, 0.25),
@@ -38,6 +46,8 @@ vec2 points[] = {
     vec2(-0.25, -0.25),
 };
 
+// All points are white so they show up on the black background and
+// multi-colored parallelogram
 vec4 point_colors[] = {
     vec4(1.0, 1.0, 1.0, 1.0),
     vec4(1.0, 1.0, 1.0, 1.0),
@@ -46,31 +56,35 @@ vec4 point_colors[] = {
     vec4(1.0, 1.0, 1.0, 1.0)
 };
 
-// Triangles
+// Right Triangle - Left side of Parallelogram
 vec2 triangle[] = {
     vec2(-1, -0.5),
     vec2(-0.5, 0.5),
     vec2(-0.5, -0.5)
 };
 
+// Triangle Colors
 vec4 tri_colors[] = {
     vec4(0.0, 1.0, 1.0, 1.0),
     vec4(0.0, 1.0, 0.0, 1.0),
     vec4(1.0, 0.0, 0.0, 1.0)
 };
 
+// Right Triangle - Right side of Parallelogram
 vec2 triangle2[] = {
     vec2(0.5, 0.5),
     vec2(1, 0.5),
     vec2(0.5, -0.5)
 };
 
+// Right side triangle colors
 vec4 tri2_colors[] = {
     vec4(0.0, 0.0, 1.0, 1.0),
     vec4(1.0, 1.0, 0.0, 1.0),
     vec4(0.0, 0.0, 0.0, 1.0)
 };
 
+// Center rectangle of Parallelogram
 vec2 rectangle[] = {
     vec2(-0.5, -0.5),
     vec2(-0.5, 0.5),
@@ -80,6 +94,7 @@ vec2 rectangle[] = {
     vec2(-0.5, -0.5)
 };
 
+// Vertex colors of rectangle
 vec4 rect_colors[] = {
     vec4(1.0, 0.0, 0.0, 1.0),
     vec4(0.0, 1.0, 0.0, 1.0),
@@ -89,47 +104,63 @@ vec4 rect_colors[] = {
     vec4(1.0, 0.0,0.0, 1.0)
 };
 
-// Create two vertex array object---OpenGL needs this to manage the Vertex
-// Buffer Object
+// Create multiple vertex array objects
 GLuint vao[5];
 
-// Create and initialize a buffer object---that's the memory buffer that
-// will be on the card!
+// Create multiple vertex buffer objects
 GLuint buffer[5];
 
 void init(void)
 {
     //
+    // This program uses a seperate VAO and VBO for each primative it
+    // renders. Though this is not at all efficient, it simplifies 
+    // the glBufferData calls such that they don't run on, and on.
+    //
+
+    //
     // ---- SHADERS ----
     //
 
-    // Per vertex coloring
+    // Initialize a vertex and fragment shader.
     GLuint program = InitShader("vshader32.glsl", "fshader32.glsl");
+    // Use the shaders. This call could come before glDraw* to switch
+    // out shaders.
     glUseProgram(program);
+    // Get a pointer to the vPosition and vColor shader attributes
     GLuint pos = glGetAttribLocation(program, "vPosition");
     GLuint col = glGetAttribLocation(program, "vColor");
 
-    // Generate two vertex arrays and two buffers
+    // Generate multiple vertex array {objects,buffers}
     glGenVertexArrays(5, vao);
     glGenBuffers(5, buffer);
 
     //
-    // ---- Triangle ----
+    // ---- First Triangle (Left Side) ----
     //
 
+    // Bind the first vertex array object and buffer.
     glBindVertexArray(vao[0]);
     glBindBuffer(GL_ARRAY_BUFFER, buffer[0]);
+    // Set aside enought memory for the points and colors.
     glBufferData(GL_ARRAY_BUFFER, sizeof(triangle)+sizeof(tri_colors), NULL, GL_STATIC_DRAW);
+    // Load the point data
     glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(triangle), triangle);
+    // Load the color data
     glBufferSubData(GL_ARRAY_BUFFER, sizeof(triangle), sizeof(tri_colors), tri_colors);
+    // Tell OpenGL to use the vPosition attribute for this VAO
     glEnableVertexAttribArray(pos);
+    // Let it know that the vPosition should read in vec2s from the
+    // VBO.
     glVertexAttribPointer(pos, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
 
+    // Let the vertex shader know to look for vec4s from the VBO at
+    // offset sizeof(triangle).
     glEnableVertexAttribArray(col);
     glVertexAttribPointer(col, 4, GL_FLOAT, GL_FALSE, 0,  BUFFER_OFFSET(sizeof(triangle)));
 
     // 
-    // ---- Triangle2 ----
+    // ---- Second Triangle (Right Side) ----
     //
     //
     glBindVertexArray(vao[1]);
