5bb9322f9cfd8f78d3c15fedb89cc239888899b0
Trevor Bramwell <trevor@bramwell.net>
2014-10-21 20:08:15 -0700

    Update assignment2 with parser and render teapot.

--- CS450/assignments/assignment2/src/assignment2.cpp
+++ CS450/assignments/assignment2/src/assignment2.cpp
@@ -4,102 +4,98 @@
 // Include the vector and matrix utilities from the textbook, as well as some
 // macro definitions.
 #include "Angel.h"
+#include "objParser.h"
 #include <stdio.h>
 
 typedef Angel::vec4  color4;
 typedef Angel::vec4  point4;
-const int NumVertices = 36; //(6 faces)(2 triangles/face)(3 vertices/triangle)
 
 GLuint  model_view;  // model-view matrix uniform shader variable location
 GLuint  projection; // projection matrix uniform shader variable location
 
+ParserState ps;
 
-point4 points[NumVertices];
-vec4   normals[NumVertices];
-
-// Vertices of a unit cube centered at origin, sides aligned with axes
-point4 vertices[8] = {
-    point4( -0.5, -0.5,  0.5, 1.0 ),
-    point4( -0.5,  0.5,  0.5, 1.0 ),
-    point4(  0.5,  0.5,  0.5, 1.0 ),
-    point4(  0.5, -0.5,  0.5, 1.0 ),
-    point4( -0.5, -0.5, -0.5, 1.0 ),
-    point4( -0.5,  0.5, -0.5, 1.0 ),
-    point4(  0.5,  0.5, -0.5, 1.0 ),
-    point4(  0.5, -0.5, -0.5, 1.0 )
-};
-
-
-// quad generates two triangles for each face and assigns colors to the
-// vertices. Notice we keep the relative ordering when constructing the
-// tris
-int Index = 0;
 void
-quad( int a, int b, int c, int d )
-{
+printParserState(ParserState* ps) {
+    printf("Vertices: %ld\n", ps->vertices.size());
+    printf("Normals: %ld\n", ps->normals.size());
+    printf("Indexes: %ld\n", (ps->indexes.size()/3));
+    printf("lines: %d\n", ps->line);
 
-
-  vec4 u = vertices[b] - vertices[a];
-  vec4 v = vertices[c] - vertices[b];
-
-  vec4 normal = normalize( cross(u, v) );
-  normal[3] = 0.0;
-
-  normals[Index] = normal; points[Index] = vertices[a]; Index++;
-  normals[Index] = normal; points[Index] = vertices[b]; Index++;
-  normals[Index] = normal; points[Index] = vertices[c]; Index++;
-  normals[Index] = normal; points[Index] = vertices[a]; Index++;
-  normals[Index] = normal; points[Index] = vertices[c]; Index++;
-  normals[Index] = normal; points[Index] = vertices[d]; Index++;
+    for (unsigned int i = 0; i < ps->vertices.size(); ++i) {
+        printf("v %f %f %f\n", ps->vertices[i][0], ps->vertices[i][1], ps->vertices[i][2]);
+    }
+    for (unsigned int i = 0; i < ps->normals.size(); ++i) {
+        printf("vn %f %f %f\n", ps->normals[i][0], ps->normals[i][1], ps->normals[i][2]);
+    }
+    for (unsigned int i = 0; i < ps->indexes.size()/3; ++i) {
+        printf("f %d %d %d\n", ps->indexes[i*3+0], ps->indexes[i*3+1], ps->indexes[i*3+2]);
+    }
 }
 
-// generate 12 triangles: 36 vertices and 36 colors
-void
-colorcube()
-{
-  quad( 4, 5, 6, 7 );
-  quad( 5, 4, 0, 1 );
-  quad( 1, 0, 3, 2 );
-  quad( 2, 3, 7, 6 );
-  quad( 3, 0, 4, 7 );
-  quad( 6, 5, 1, 2 );
+int
+vec_size(std::vector<vec4> v) {
+    return sizeof(v[0])*v.size();
+}
+
+int
+vec_size_int(std::vector<int> v) {
+    return sizeof(v[0])*v.size();
 }
 
 // OpenGL initialization
 void
-init()
+init(ParserState* ps)
 {
-    colorcube();
+
+    // Load shaders and use the resulting shader program
+    GLuint program = InitShader("vshader.glsl", "fshader.glsl");
+    glUseProgram(program);
 
     // Create a vertex array object
     GLuint vao;
-    glGenVertexArrays( 1, &vao );
-    glBindVertexArray( vao );
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
 
     // Create and initialize a buffer object
-    GLuint buffer;
-    glGenBuffers( 1, &buffer );
-    glBindBuffer( GL_ARRAY_BUFFER, buffer );
-    glBufferData( GL_ARRAY_BUFFER, sizeof(points) + sizeof(normals),
-		  NULL, GL_STATIC_DRAW );
-    glBufferSubData( GL_ARRAY_BUFFER, 0, sizeof(points), points );
-    glBufferSubData( GL_ARRAY_BUFFER, sizeof(points), sizeof(normals), normals );
+    GLuint vbo;
+    glGenBuffers( 1, &vbo );
+    glBindBuffer( GL_ARRAY_BUFFER, vbo );
+    glBufferData( GL_ARRAY_BUFFER,
+                  vec_size(ps->vertices) + vec_size(ps->normals),
+                  NULL,
+                  GL_STATIC_DRAW );
+    glBufferSubData( GL_ARRAY_BUFFER,
+                     0,
+                     vec_size(ps->vertices),
+                     &ps->vertices[0] );
+    glBufferSubData( GL_ARRAY_BUFFER,
+                     vec_size(ps->vertices),
+                     vec_size(ps->normals),
+                     &ps->normals[0] );
 
-    // Load shaders and use the resulting shader program
-    GLuint program = InitShader( "vshader.glsl", "fshader.glsl" );
-    glUseProgram( program );
+    // Create an Element Array Buffer
+    GLuint ebo;
+    glGenBuffers( 1, &ebo );
+    glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, ebo );
+    glBufferData( GL_ELEMENT_ARRAY_BUFFER,
+                  ps->indexes.size()*sizeof(&ps->indexes[0]),
+                  &ps->indexes[0],
+                  GL_STATIC_DRAW );
 
     // set up vertex arrays
     GLuint vPosition = glGetAttribLocation( program, "vPosition" );
     glEnableVertexAttribArray( vPosition );
-    glVertexAttribPointer( vPosition, 4, GL_FLOAT, GL_FALSE, 0,
-			   BUFFER_OFFSET(0) );
+    glVertexAttribPointer( vPosition, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0) );
 
     GLuint vNormal = glGetAttribLocation( program, "vNormal" );
     glEnableVertexAttribArray( vNormal );
-    glVertexAttribPointer( vNormal, 4, GL_FLOAT, GL_FALSE, 0,
-			   BUFFER_OFFSET(sizeof(points)) );
-
+    glVertexAttribPointer( vNormal,
+                           4,
+                           GL_FLOAT,
+                           GL_FALSE,
+                           0,
+                           BUFFER_OFFSET( (long int)vec_size(ps->vertices) ) );
 
     // Initialize shader lighting parameters
     // RAM: No need to change these...we'll learn about the details when we
@@ -136,11 +132,23 @@ init()
     projection = glGetUniformLocation( program, "Projection" );
 
 
+    // Sphere
+    //mat4 p = Perspective(90.0, 1.0, 0.1, 8.0);
+    //point4  eye( 0.0, 0.0, 2.0, 1.0);
+    //point4  at( 0.0, 0.0, 0.0, 1.0 );
+    //vec4    up( 0.0, 1.0, 0.0, 0.0 );
 
-    mat4 p = Perspective(45, 1.0, 0.1, 10.0);
-    point4  eye( 1.0, 1.0, 2.0, 1.0);
+    // Teapot
+    mat4 p = Perspective(45.0, 1.0, 0.1, 10.0);
+    point4  eye( 0.0, 0.0, 2.0, 1.0);
     point4  at( 0.0, 0.0, 0.0, 1.0 );
     vec4    up( 0.0, 1.0, 0.0, 0.0 );
+
+    // Teapot & Bunny
+    //mat4 p = Perspective (60.0, 1.0, 0.1, 15.0);
+    //point4  eye( 0.0, 0.0, 0.3, 1.0);
+    //point4  at( 0.0, 1.0, 0.0, 1.0 );
+    //vec4    up( 0.0, 1.0, 0.0, 0.0 );
 
 
     mat4  mv = LookAt( eye, at, up );
@@ -157,7 +165,7 @@ void
 display( void )
 {
     glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-    glDrawArrays( GL_TRIANGLES, 0, NumVertices );
+    glDrawElements( GL_TRIANGLES, ps.vertices.size()*6, GL_UNSIGNED_INT, 0 );
     glutSwapBuffers();
 }
 
@@ -176,7 +184,7 @@ keyboard( unsigned char key, int x, int y )
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
     glutInitContextVersion (3, 2);
     glutInitContextFlags (GLUT_FORWARD_COMPATIBLE);
     glutInitWindowSize(512, 512);
@@ -187,7 +195,16 @@ int main(int argc, char** argv)
     glewExperimental = GL_TRUE;
     glewInit();
 
-    init();
+    // Parse obj
+    //parse(&ps, "../assets/sphere42NS.obj");
+    parse(&ps, "../assets/teapotNS.obj");
+    //parse(&ps, "../assets/bunnyNS.obj");
+
+    // Debug
+    //printParserState(&ps);
+
+    // Pass to init
+    init(&ps);
 
     //NOTE:  callbacks must go after window is created!!!
     glutKeyboardFunc(keyboard);
