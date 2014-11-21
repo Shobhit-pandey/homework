c8c0fd9f9ba9e0113397de51e5f2592a980f9504
Trevor Bramwell <trevor@bramwell.net>
2014-11-21 09:03:44 -0800

    Discratize diffuse lighting for cel shader.

--- CS450/assignments/assignment5/src/celfragment.glsl
+++ CS450/assignments/assignment5/src/celfragment.glsl
@@ -16,6 +16,19 @@ vec3 material_specular = vec3(1.0, 0.8, 0.0);
 
 out vec4 color;
 
+float discratize(float normal_diffuse) {
+    if( normal_diffuse <= 0.2) {
+        return 0.0;
+    } else if( normal_diffuse <= 0.4) {
+        return 0.25;
+    } else if( normal_diffuse <= 0.6) {
+        return 0.4;
+    } else {
+        return 0.8;
+    }
+    return 1.0;
+}
+
 void main()
 {
     vec3 L = normalize(light - pos); // vector from point to light
@@ -23,8 +36,10 @@ void main()
     vec3 R = reflect(-L, N); // Relfect around normal. (Positive would highlight shadows)
     vec3 V = normalize(-pos); // vector towards the viewer
 
+    float normal_diffuse = max(dot(L,N), 0.0);
+
     vec3 ambient = light_ambient * material_ambient;
-    vec3 diffuse = max(dot(L, N), 0.0) * light_diffuse * material_diffuse;
+    vec3 diffuse = discratize(normal_diffuse) * light_diffuse * material_diffuse;
     vec3 specular = pow(max(dot(R, V), 0.0), 100.0) * light_specular * material_specular;
 
     vec3 light_color = ambient + diffuse + specular;
