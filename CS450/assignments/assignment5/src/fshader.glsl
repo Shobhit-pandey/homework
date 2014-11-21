56963754c6bd9942fea2a4443ce403a1505c8d0f
Trevor Bramwell <trevor@bramwell.net>
2014-11-20 19:36:07 -0800

    Switch from Gouruad to Phong shading.

--- CS450/assignments/assignment5/src/fshader.glsl
+++ CS450/assignments/assignment5/src/fshader.glsl
@@ -1,9 +1,49 @@
 #version 150
 
-in  vec4 color;
-out vec4 fColor;
+in vec4 vColor;
+in vec3 pos;
+in vec3 normal;
+in vec3 light;
+
+uniform int Swap;
+
+// Define Illumination Constants
+vec3 light_ambient  = vec3(0.2);
+vec3 light_diffuse  = vec3(1.0);
+vec3 light_specular = vec3(1.0);
+
+vec3 material_ambient  = vec3(1.0, 0.0, 1.0);
+vec3 material_diffuse  = vec3(1.0, 0.8, 0.0);
+vec3 material_specular = vec3(1.0, 0.8, 0.0);
+
+out vec4 color;
 
 void main()
 {
-    fColor = color;
+    vec3 L = normalize(light - pos);
+    vec3 N = normalize(normal);
+    vec3 R = reflect(-L, N);
+    vec3 V = normalize(-pos);
+
+    //vec3 normal;
+    //vec3 light;
+    //vec3 viewer;
+    //vec3 reflection;
+
+    // Transform vertex normal into eye coordinates
+    //   To correctly transform normals use first formation
+    // This should just be view, that way when the model rotates, the
+    // light does not.
+    // vec3 N = (normalize (transpose (inverse (ModelView))*vNormal).xyz);
+
+    vec3 ambient = light_ambient * material_ambient;
+    vec3 diffuse = max(dot(L, N), 0.0) * light_diffuse * material_diffuse;
+    vec3 specular = pow(max(dot(R, V), 0.0), 100.0) * light_specular * material_specular;
+
+    if (Swap == 0) {
+        vec3 light_color = ambient + diffuse + specular;
+        color = vec4(light_color, 1.0);
+    } else {
+        color = vColor;
+    }
 }
--- CS450/assignments/assignment5/src/vshader.glsl
+++ CS450/assignments/assignment5/src/vshader.glsl
@@ -2,66 +2,23 @@
 
 in  vec4 vPosition;
 in  vec4 vNormal;
-in  vec4 vColor;
-out vec4 color;
 
 uniform mat4 Transform;
 uniform mat4 ModelView;
 uniform mat4 Projection;
-uniform int Swap;
+
+vec4 vLight = vec4(1.5, 1.5, 2.0, 1.0);
+
+out vec3 pos;
+out vec3 normal;
+out vec3 light;
 
 void main()
 {
-    // Define Illumination Constants
-    vec3 light_ambient  = vec3(0.2);
-    vec3 light_diffuse  = vec3(1.0);
-    vec3 light_specular = vec3(1.0);
-
-    vec3 material_ambient  = vec3(1.0, 0.0, 1.0);
-    vec3 material_diffuse  = vec3(1.0, 0.8, 0.0);
-    vec3 material_specular = vec3(1.0, 0.8, 0.0);
-
-    vec4 LightPosition = vec4(1.5, 1.5, 2.0, 1.0);
-
-    vec3 AmbientProduct  = light_ambient  * material_ambient;
-    vec3 DiffuseProduct  = light_diffuse  * material_diffuse;
-    vec3 SpecularProduct = light_specular * material_specular;
-
-    //vec3 normal;
-    //vec3 light;
-    //vec3 viewer;
-    //vec3 reflection;
-
-    // Transform vertex  position into eye coordinates
-    vec3 pos = (ModelView * vPosition).xyz;
-
-    vec3 L = normalize( (ModelView * LightPosition).xyz - pos );
-    vec3 E = normalize( -pos );
-    vec3 H = normalize( L + E );  //halfway vector
-
-    // Transform vertex normal into eye coordinates
-    vec3 N = normalize( ModelView*vNormal ).xyz;
-
-    //To correctly transform normals
-    // vec3      N = (normalize (transpose (inverse (ModelView))*vNormal).xyz
-
-    float dr = max( dot(L, N), 0.0 );
-    vec3  diffuse = dr * DiffuseProduct;
-
-    // Power constant represents 'shininess'
-    float sr = pow( max(dot(N, H), 0.0), 100.0 );
-    vec3 specular = sr * SpecularProduct;
-
-    if( dot(L, N) < 0.0 ) {
-        specular = vec3(0.0, 0.0, 0.0);
-    }
+    // Transform position, normals, and light into eye coordinates
+    pos    = vec3(ModelView * vPosition);
+    normal = vec3(ModelView * vNormal);
+    light  = vec3(ModelView * vLight);
 
     gl_Position = Projection * ModelView * Transform * vPosition;
-
-    if (Swap == 0) {
-        vec3 light_color = AmbientProduct + diffuse + specular;
-        color = vec4(light_color, 1.0);
-    } else {
-        color = vColor;
-    }
 }
